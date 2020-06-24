d7c83957cad568f22ebe86872f402e8ac7b05dcc
Shikhar Kumar <shkumar@tripadvisor.com>
2020-06-24 15:38:54 -0400

    adding basic routing and user mongoose schema

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect to database
+connectDB();
+
 app.get('/', (req, res) => res.send('Api running!'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+// Listener
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
