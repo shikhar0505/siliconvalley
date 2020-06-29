61d35d1b9f67e69c6d61c15ef6061f3300cfb274
Shikhar Kumar <shkumar@tripadvisor.com>
2020-06-29 14:48:37 -0400

    adding api for liking and removing likes from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -108,4 +108,67 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({message: 'Post not found.'});
+    }
+
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+      return res.status(400).json({ message: 'Post already liked.' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind == 'ObjectId') {
+      return res.status(404).json({
+        message: 'Post not found.'
+      });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Remove a like on a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({message: 'Post not found.'});
+    }
+
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ message: 'Post has not yet been liked.' });
+    }
+
+    // get remove index
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind == 'ObjectId') {
+      return res.status(404).json({
+        message: 'Post not found.'
+      });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
