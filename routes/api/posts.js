9b5599fa34fa526c711b66ead4d68c328102d158
Shikhar Kumar <shkumar@tripadvisor.com>
2020-06-29 15:20:24 -0400

    adding routes to add/delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -31,7 +31,7 @@ router.post('/', [ auth, [
       text: req.body.text,
       name: user.name,
       avatar: user.avatar,
-      user: user.id
+      user: req.user.id
     });
     const post = await newPost.save();
 
@@ -160,6 +160,84 @@ router.put('/unlike/:id', auth, async (req, res) => {
     await post.save();
 
     res.json(post.likes);
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind == 'ObjectId') {
+      return res.status(404).json({
+        message: 'Post not found.'
+      });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post('/comment/:id', [ auth, [
+  check('text', 'Text is required').not().isEmpty()
+] ], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+    post.comments.unshift(newComment);
+    await post.save();
+
+    res.json(post.comments);
+  } catch (e) {
+    console.error(e.message);
+    if (e.kind == 'ObjectId') {
+      return res.status(404).json({
+        message: 'Post not found.'
+      });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({message: 'Post not found.'});
+    }
+
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+    if (!comment) {
+      return res.status(404).json({message: 'Comment not found.'});
+    }
+
+    // check if the user deleting the commment owns the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'User not authorized.' });
+    }
+
+    // get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
   } catch (e) {
     console.error(e.message);
     if (e.kind == 'ObjectId') {
