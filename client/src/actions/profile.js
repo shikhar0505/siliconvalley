df6bd012b0f902df7802a12e43bae40aca854e3e
Shikhar Kumar <shkumar@tripadvisor.com>
2020-06-30 17:34:21 -0400

    added actions/components to delete education, experience and entire account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,7 +4,9 @@ import { setAlert } from './alert';
 import {
   GET_PROFILE,
   UPDATE_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
 } from './types';
 
 // Get current user's profile
@@ -133,5 +135,71 @@ export const addEducation = (formData, history) => async dispatch => {
         status: e.response.status
       }
     });
+  }
+};
+
+// Delete experience
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (e) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: e.response.statusText,
+        status: e.response.status
+      }
+    });
+  }
+};
+
+// Delete education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (e) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: e.response.statusText,
+        status: e.response.status
+      }
+    });
+  }
+};
+
+// Delete account and profile
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      await axios.delete('/api/profile');
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been permanently deleted'));
+    } catch (e) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: e.response.statusText,
+          status: e.response.status
+        }
+      });
+    }
   }
 };
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,10 +6,11 @@ import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading }
 }) => {
@@ -28,6 +29,11 @@ const Dashboard = ({
           <DashboardActions />
           <Experience experience={ profile.experience } />
           <Education education={ profile.education } />
+          <div className="my-2">
+            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+              <i className="fas fas-user-minus"></i> Delete My Account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
@@ -43,6 +49,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 };
@@ -52,6 +59,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
   Dashboard
 );
